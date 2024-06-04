5a57bda36b75fc8fcca7c6dda76a16bdb168150c
임진조 <dlawlswh123@gmail.com>
2024-06-04 19:30:06 +0900

    SVG 파일 적용 세팅

--- pandamarket-ts/next.config.mjs
+++ pandamarket-ts/next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
