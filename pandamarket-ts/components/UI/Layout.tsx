754c2b1e50b8457aa6cd277216db4723b3d55356
임진조 <dlawlswh123@gmail.com>
2024-06-06 18:02:57 +0900

    Style: Layout 수정

--- pandamarket-ts/components/UI/Layout.tsx
+++ pandamarket-ts/components/UI/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="mx-auto max-w-[1200px]">
+    <div className="mx-auto max-w-[1200px] sm:px-[16px] md:px-[24px]">
       <Header />
       <h1>레이아웃</h1>
       {children}
